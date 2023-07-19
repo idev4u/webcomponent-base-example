06a1a241edd4806f1de3f55061a05b83edfe8bd6
Norman Sutorius <norman.sutorius@porsche.de>
2023-07-19 14:42:12 +0200

    the message is personalised

--- hello-world.js
+++ hello-world.js
@@ -19,9 +19,15 @@ class HelloCard extends HTMLElement{
     connectedCallback(){
         this.$card = this.querySelector('.card-body');
         let $message = document.createElement('p');
-        $message.innerHTML = `Message: hello party people`;
+        let name = this.nameOfPersonAddressed;
+        $message.innerHTML = `Message: hello ${name}`;
         this._shadowRoot.appendChild($message);
     }
+    
+    // add attributes reader
+    get nameOfPersonAddressed(){
+        console.log(JSON.stringify(this.getAttribute('name')));
+        return this.getAttribute('name');
+    }
 }
-
 window.customElements.define('hello-card', HelloCard);
